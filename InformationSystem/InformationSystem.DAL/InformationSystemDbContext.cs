cd192e2f18098dd0f26cd63c1ac6ed196dbec8b9
Maksim Dubrovin <maximus27rus@gmail.com>
2024-03-03 15:22:54 +0100

    Removed unnecessary imports.

--- InformationSystem/InformationSystem.DAL/InformationSystemDbContext.cs
+++ InformationSystem/InformationSystem.DAL/InformationSystemDbContext.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using InformationSystem.DAL.Entities;
+﻿using InformationSystem.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace InformationSystem.DAL {
+namespace InformationSystem.DAL 
+{
     public class InformationSystemDbContext(DbContextOptions options) : DbContext(options) {
         public DbSet<ActivityEntity> Activities => Set<ActivityEntity>();
         public DbSet<CourseEntity> Courses => Set<CourseEntity>();
