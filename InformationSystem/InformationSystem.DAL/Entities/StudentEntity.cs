147f29cfd70ce972668a3d0cce92d4c31d78355e
Maksim Dubrovin <maximus27rus@gmail.com>
2024-03-03 14:21:13 +0100

    Refactored entities classes (added annotations, and common interface).

--- InformationSystem/InformationSystem.DAL/Entities/StudentEntity.cs
+++ InformationSystem/InformationSystem.DAL/Entities/StudentEntity.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace InformationSystem.DAL.Entities {
-    public class StudentEntity 
-    {
-        public Guid Id { get; set; } = Guid.NewGuid(); 
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public string? ImageUrl { get; set; }
-        public required string Group { get; set; }
-        public int CurrentYear { get; set; } 
+namespace InformationSystem.DAL.Entities;
 
-        public Guid CourseId { get; set; }
-        public ICollection<CourseEntity> ChosenCourses { get; init; } = new List<CourseEntity>();
-    }
+public class StudentEntity 
+{
+    public Guid Id { get; set; } = Guid.NewGuid(); 
+    [MaxLength(50)]
+    public required string FirstName { get; set; }
+    [MaxLength(50)]
+    public required string LastName { get; set; }
+    public string? ImageUrl { get; set; }
+    [MaxLength(5)]
+    public required string Group { get; set; }
+    public int CurrentYear { get; set; } 
+
+    public Guid CourseId { get; set; }
+    public ICollection<CourseEntity> ChosenCourses { get; init; } = new List<CourseEntity>();
 }
