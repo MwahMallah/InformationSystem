aeb0a1c58d8772bd49eeaeb7b1a6ae1bb0cbefe0
Maksim Dubrovin <maximus27rus@gmail.com>
2024-04-03 23:50:52 +0200

    Added generic Repository class to abstract CRUD operations under all Entities.

---
+++ InformationSystem/InformationSystem.DAL/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using InformationSystem.DAL.Entities;
+using InformationSystem.DAL.Mappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace InformationSystem.DAL.Repositories;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
+{
+    private readonly DbSet<TEntity> _dbSet;
+    private IEntityMapper<TEntity> _entityMapper;
+    
+    public Repository(DbContext dbContext, IEntityMapper<TEntity> entityMapper)
+    {
+        _entityMapper = entityMapper;
+        _dbSet = dbContext.Set<TEntity>();
+    }
+    
+    public async Task<TEntity> InsertAsync(TEntity entity)
+    {
+        return (await _dbSet.AddAsync(entity)).Entity;
+    }
+
+    public IQueryable<TEntity> Get()
+    {
+        return _dbSet.AsQueryable();
+    }
+    
+    public async ValueTask<bool> ExistsAsync(TEntity entity)
+    {
+        return entity.Id != Guid.Empty && 
+               (await _dbSet.AnyAsync(e => e.Id == entity.Id));
+    }
+
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        var existingEntity = await _dbSet.SingleAsync(e => e.Id == entity.Id);
+        _entityMapper.MapToExistingEntity(existingEntity, entity);
+        return existingEntity;
+    }
+
+    public void Delete(Guid entityId)
+    {
+        _dbSet.Remove(_dbSet.Single(e => e.Id == entityId));
+    }
+}
