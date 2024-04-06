7746f1c79831fb8ca082bbd4674baa5e8345fe12
Ilya Volkov <2003volkovi@gmail.com>
2024-04-06 23:19:29 +0200

    ActivitySeeds.cs and EvaluationSeeds.cs were added and filled with some content

--- InformationSystem/InformationSystem.Common.Tests/Seeds/ActivitySeeds.cs
+++ InformationSystem/InformationSystem.Common.Tests/Seeds/ActivitySeeds.cs
@@ -1,8 +1,29 @@
-﻿using InformationSystem.DAL.Entities;
+﻿using InformationSystem.Common.Enums;
+using InformationSystem.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace InformationSystem.Common.Tests.Seeds;
 
 public static class ActivitySeeds
 {
+    public static readonly ActivityEntity EmptyActivityEntity = new()
+    {
+        Id = default,
+        ActivityType = default,
+        RoomType = default,
+        Course = new CourseEntity() {Id = default, Name = "", Abbreviation = ""},
+    };
+    public static readonly ActivityEntity ActivityEntity = new()
+    {
+        Id = Guid.NewGuid(),
+        ActivityType = ActivityType.Exam,
+        RoomType = RoomType.ComputerClass,
+        Course = new CourseEntity() {Id = Guid.NewGuid(), Name = "Databases", Abbreviation = "IDS"},
+    };
+    public static void Seed(this ModelBuilder modelBuilder) =>
+        modelBuilder.Entity<ActivityEntity>().HasData(
+            ActivityEntity,
+            EmptyActivityEntity
+        );
     
 }
--- InformationSystem/InformationSystem.Common.Tests/Seeds/EvaluationSeeds.cs
+++ InformationSystem/InformationSystem.Common.Tests/Seeds/EvaluationSeeds.cs
@@ -1,8 +1,27 @@
 ﻿using InformationSystem.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace InformationSystem.Common.Tests.Seeds;
 
 public static class EvaluationSeeds
 {
-    
+    public static readonly EvaluationEntity EmptyEvaluationEntity = new()
+    {
+        Id = default,
+        Description = "",
+        Points = default,
+        Student = new() { Id = default, FirstName = "", LastName = "", Group = "" },
+        Activity = new(){Id = default, Course = new(){Id = default, Name = "", Abbreviation = ""}}
+    };
+    public static readonly EvaluationEntity EvaluationEntity = new()
+    {
+        Id = Guid.NewGuid(),
+        Description = "exam eval.",
+        Points = 10,
+        Student = new() { Id = default, FirstName = "Alex", LastName = "Mercy", Group = "1C" },
+        Activity = new(){Id = Guid.NewGuid(), Course = new(){Id = Guid.NewGuid(), Name = "programming", Abbreviation = "IZP"}}
+    };
+
+    public static void Seed(this ModelBuilder modelBuilder) =>
+        modelBuilder.Entity<EvaluationEntity>().HasData(EvaluationEntity);
 }
