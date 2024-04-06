828c8888e3cf766c576fe3eb7be32b58f63964be
Maksim Dubrovin <maximus27rus@gmail.com>
2024-04-06 17:34:39 +0200

    Added interface, base class and all classes for Facades.

---
+++ InformationSystem/InformationSystem.BL/Facades/StudentFacade.cs
@@ -0,0 +1,16 @@
+﻿using InformationSystem.BL.Mappers;
+using InformationSystem.BL.Models;
+using InformationSystem.DAL.Entities;
+using InformationSystem.DAL.Mappers;
+using InformationSystem.DAL.UnitOfWork;
+
+namespace InformationSystem.BL.Facades;
+
+public class StudentFacade(
+    IUnitOfWorkFactory unitOfWorkFactory, 
+    IModelMapper<StudentEntity, StudentDetailModel, StudentListModel> modelMapper) 
+    : FacadeBase<StudentEntity, StudentDetailModel, StudentListModel, StudentEntityMapper>
+        (unitOfWorkFactory, modelMapper)
+{
+    
+}
