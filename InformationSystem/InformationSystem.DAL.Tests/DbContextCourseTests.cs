89c70419e7b8e8a7ff69ea2dd9ec82912a609384
Ilya Volkov <2003volkovi@gmail.com>
2024-04-06 18:28:14 +0200

    created DbContextCourseTests.cs

---
+++ InformationSystem/InformationSystem.DAL.Tests/DbContextCourseTests.cs
@@ -0,0 +1,12 @@
+﻿using InformationSystem.Common.Tests.Seeds;
+using Microsoft.EntityFrameworkCore;
+using Xunit.Abstractions;
+using Xunit;
+
+namespace InformationSystem.DAL.Tests
+{
+    public class DbContextCourseTests(ITestOutputHelper output) : DbContextTestsBase(output)
+    {
+
+    }
+}
