785d7b99271399c1c41dc37108af292ce8f80c31
Ilya Volkov <2003volkovi@gmail.com>
2024-04-07 00:39:09 +0200

    DbContextEvaluationTests.cs and DbContextEvaluationTests.cs were added and filled with tests

---
+++ InformationSystem/InformationSystem.DAL.Tests/DbContextActivityTests.cs
@@ -0,0 +1,88 @@
+﻿using InformationSystem.Common.Enums;
+using InformationSystem.Common.Tests.Seeds;
+using Microsoft.EntityFrameworkCore;
+using Xunit.Abstractions;
+using Xunit;
+using InformationSystem.Common.Tests;
+using InformationSystem.DAL.Entities;
+
+namespace InformationSystem.DAL.Tests;
+
+public class DbContextActivityTests(ITestOutputHelper output) : DbContextTestsBase(output)
+{
+    [Fact]
+    public async Task AddNew_Activity_Persisted()
+    {
+        var activity = ActivitySeeds.ActivityEntity;
+
+        InformationSystemDbContextSUT.Activities.Add(activity);
+        await InformationSystemDbContextSUT.SaveChangesAsync();
+
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var dbActivity = await dbx.Activities
+            .SingleAsync(i => i.Id == activity.Id);
+        DeepAssert.Equal(activity, dbActivity);
+    }
+
+    [Fact]
+    public async Task GetById_Activity()
+    {
+        var activity = await InformationSystemDbContextSUT.Activities
+            .SingleAsync(i => i.Id == ActivitySeeds.ActivityEntity.Id);
+
+        //Assert
+        DeepAssert.Equal(ActivitySeeds.ActivityEntity, activity);
+    }
+
+    [Fact]
+    public async Task Update_Activity_Persisted()
+    {
+        var baseEntity = ActivitySeeds.ActivityEntity;
+        var entity =
+            baseEntity with
+            {
+                ActivityType = ActivityType.Exercise,
+                RoomType = RoomType.MathClass,
+            };
+
+        InformationSystemDbContextSUT.Activities.Update(entity);
+        await InformationSystemDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var actualEntity = await dbx.Activities.SingleAsync(i => i.Id == entity.Id);
+        DeepAssert.Equal(entity, actualEntity);
+    }
+
+    [Fact]
+    public async Task Delete_Activity_Deleted()
+    {
+        var baseEntity = ActivitySeeds.ActivityEntity;
+
+        InformationSystemDbContextSUT.Activities.Remove(baseEntity);
+        await InformationSystemDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        Assert.False(await InformationSystemDbContextSUT.Activities.AnyAsync(i => i.Id == baseEntity.Id));
+    }
+
+    [Fact]
+    public async Task GetAll_Activities_ContainsSeededActivity()
+    {
+        var activities = await InformationSystemDbContextSUT.Activities.ToListAsync();
+
+        //Assert
+        DeepAssert.Contains(ActivitySeeds.ActivityEntity, activities);
+    }
+
+    [Fact]
+    public async Task GetById_IncludingCourse_Activity()
+    {
+        var entity = await InformationSystemDbContextSUT.Activities
+            .Include(i => i.Course)
+            .SingleAsync(i => i.Id == ActivitySeeds.ActivityEntity.Id);
+
+        //Assert
+        DeepAssert.Equal(ActivitySeeds.ActivityEntity, entity);
+    }
+}
---
+++ InformationSystem/InformationSystem.DAL.Tests/DbContextEvaluationTests.cs
@@ -0,0 +1,68 @@
+﻿using InformationSystem.Common.Tests.Seeds;
+using Microsoft.EntityFrameworkCore;
+using Xunit.Abstractions;
+using Xunit;
+using InformationSystem.Common.Tests;
+using InformationSystem.DAL.Entities;
+
+namespace InformationSystem.DAL.Tests;
+
+public class DbContextEvaluationTests(ITestOutputHelper output) : DbContextTestsBase(output)
+{
+    [Fact]
+    public async Task AddNew_Evaluation_Persisted()
+    {
+        var evaluation = EvaluationSeeds.EvaluationEntity;
+
+        InformationSystemDbContextSUT.Evaluations.Add(evaluation);
+        await InformationSystemDbContextSUT.SaveChangesAsync();
+
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var dbEvaluation = await dbx.Evaluations
+            .SingleAsync(i => i.Id == evaluation.Id);
+        DeepAssert.Equal(evaluation, dbEvaluation);
+    }
+
+    [Fact]
+    public async Task GetById_Evaluation()
+    {
+        var evaluation = await InformationSystemDbContextSUT.Evaluations
+            .SingleAsync(i => i.Id == EvaluationSeeds.EvaluationEntity.Id);
+
+        //Assert
+        DeepAssert.Equal(EvaluationSeeds.EvaluationEntity, evaluation);
+    }
+
+    [Fact]
+    public async Task Update_Evaluation_Persisted()
+    {
+        //Arrange
+        var baseEntity = EvaluationSeeds.EvaluationEntity;
+        var entity =
+            baseEntity with
+            {
+                Description = baseEntity.Description + "Updated",
+                Points = baseEntity.Points + 5
+            };
+
+        InformationSystemDbContextSUT.Evaluations.Update(entity);
+        await InformationSystemDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var actualEntity = await dbx.Evaluations.SingleAsync(i => i.Id == entity.Id);
+        DeepAssert.Equal(entity, actualEntity);
+    }
+
+    [Fact]
+    public async Task Delete_Evaluation_Deleted()
+    {
+        var baseEntity = EvaluationSeeds.EvaluationEntity;
+
+        InformationSystemDbContextSUT.Evaluations.Remove(baseEntity);
+        await InformationSystemDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        Assert.False(await InformationSystemDbContextSUT.Evaluations.AnyAsync(i => i.Id == baseEntity.Id));
+    }
+}
