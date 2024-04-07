9f62d48343142d4974204bb1da1d2929da52d3b1
Maksim Dubrovin <maximus27rus@gmail.com>
2024-04-07 23:30:30 +0200

    Added project settings and forgotten migrations.

---
+++ InformationSystem/InformationSystem.DAL/Migrations/20240407005736_activity_changed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InformationSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class activity_changed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_Evaluations_EvaluationId",
+                table: "Activities");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_Evaluations_EvaluationId",
+                table: "Activities",
+                column: "EvaluationId",
+                principalTable: "Evaluations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_Evaluations_EvaluationId",
+                table: "Activities");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_Evaluations_EvaluationId",
+                table: "Activities",
+                column: "EvaluationId",
+                principalTable: "Evaluations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
