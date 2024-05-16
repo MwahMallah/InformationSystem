0d3eb3a5d6e35eda75a82364d1adbcbad560944f
Maksim Dubrovin <maximus27rus@gmail.com>
2024-05-16 18:57:13 +0200

    added initial student's edit view and view model.

---
+++ InformationSystem/InformationSystem.App/ViewModels/Student/StudentEditViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using InformationSystem.App.Messages;
+using InformationSystem.App.Services;
+using InformationSystem.App.Services.Interfaces;
+using InformationSystem.BL.Facades;
+using InformationSystem.BL.Facades.Interfaces;
+using InformationSystem.BL.Models;
+
+namespace InformationSystem.App.ViewModels.Student;
+
+public partial class StudentEditViewModel(
+        IStudentFacade studentFacade,
+        INavigationService navigationService,
+        IMessengerService messengerService
+    ) : ViewModelBase(messengerService)
+{
+    public StudentDetailModel Student { get; set; } = StudentDetailModel.Empty;
+
+    [RelayCommand]
+    public async Task SaveAsync()
+    {
+        await studentFacade.SaveAsync(Student);
+        
+        MessengerService.Send(new MessageEditStudent() {StudentId = Student.Id});
+        navigationService.SendBackButtonPressed();
+    }
+}
---
+++ InformationSystem/InformationSystem.App/Views/Student/StudentEditView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InformationSystem.App.ViewModels.Student;
+
+namespace InformationSystem.App.Views.Student;
+
+public partial class StudentEditView : BaseView
+{
+    public StudentEditView(StudentEditViewModel studentEditViewModel) : base(studentEditViewModel)
+    {
+        InitializeComponent();
+    }
+}
