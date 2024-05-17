235e319aa2344bd7f17a8629500ec3d2b5ed396e
Maksim Dubrovin <maximus27rus@gmail.com>
2024-05-17 23:23:32 +0200

    Student edit view model now takes Query parameters, enabled routing from student's edit view to selector of student's courses.

--- InformationSystem/InformationSystem.App/ViewModels/Student/StudentEditViewModel.cs
+++ InformationSystem/InformationSystem.App/ViewModels/Student/StudentEditViewModel.cs
@@ -9,20 +9,32 @@ using InformationSystem.BL.Models;
 
 namespace InformationSystem.App.ViewModels.Student;
 
+[QueryProperty(nameof(Student), nameof(Student))]
 public partial class StudentEditViewModel(
         IStudentFacade studentFacade,
         INavigationService navigationService,
         IMessengerService messengerService
     ) : ViewModelBase(messengerService)
 {
-    public StudentDetailModel Student { get; set; } = StudentDetailModel.Empty;
+    [ObservableProperty]
+    private StudentDetailModel student = StudentDetailModel.Empty;
 
     [RelayCommand]
-    public async Task SaveAsync()
+    private async Task SaveAsync()
     {
         await studentFacade.SaveAsync(Student);
         
         MessengerService.Send(new MessageEditStudent() {StudentId = Student.Id});
         navigationService.SendBackButtonPressed();
+    }
+
+    [RelayCommand]
+    private async Task GoToStudentCourseEditAsync()
+    {
+        await navigationService.GoToAsync("/courses", 
+            new Dictionary<string, object?>()
+            {
+                [nameof(StudentCoursesEditViewModel.Student)] = Student
+            });
     }
 }
