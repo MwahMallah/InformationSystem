e8077e8d31aa4a40a8e5510bc6bcef2b0bd38a93
Maksim Dubrovin <maximus27rus@gmail.com>
2024-05-17 23:24:03 +0200

    Made view, where we can change student's courses.

---
+++ InformationSystem/InformationSystem.App/ViewModels/Student/StudentCoursesEditViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using InformationSystem.App.Services.Interfaces;
+using InformationSystem.BL.Facades.Interfaces;
+using InformationSystem.BL.Mappers.Interfaces;
+using InformationSystem.BL.Models;
+
+namespace InformationSystem.App.ViewModels.Student;
+
+[QueryProperty(nameof(Student), nameof(Student))]
+public partial class StudentCoursesEditViewModel
+    (IMessengerService messengerService,
+        ICourseModelMapper courseListModelMapper,
+        IStudentFacade studentFacade,
+        ICourseFacade courseFacade) 
+    : ViewModelBase(messengerService)
+{
+    [ObservableProperty] 
+    private StudentDetailModel student;
+
+    [ObservableProperty] 
+    private ObservableCollection<CourseListModel> courses = [];
+
+    public CourseListModel? SelectedCourse { get; set; } = null;
+    
+    protected override async Task LoadDataAsync()
+    {
+        var allCourses = await courseFacade.GetAsync();
+
+        Courses.Clear();
+        foreach (var course in allCourses)
+        {
+            Courses.Add(course);
+        }
+    }
+
+    [RelayCommand]
+    private async Task AddCourseToStudentAsync()
+    {
+        if (SelectedCourse != null)
+        {
+            var courseListModel = courseListModelMapper
+                .MapToListModel(await courseFacade.GetAsync(SelectedCourse.Id));
+            
+            Student.Courses.Add(courseListModel);
+            await studentFacade.SaveAsync(Student);
+        }
+    }
+}
---
+++ InformationSystem/InformationSystem.App/Views/Student/StudentCoursesEditView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InformationSystem.App.ViewModels.Student;
+
+namespace InformationSystem.App.Views.Student;
+
+public partial class StudentCoursesEditView
+{
+    public StudentCoursesEditView(StudentCoursesEditViewModel viewModel) : base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
