4befdec6bafdfdca4fe63e37bcb66333ad830c6a
Maksim Dubrovin <maximus27rus@gmail.com>
2024-05-18 13:58:55 +0200

    Made Activity Edit view.

---
+++ InformationSystem/InformationSystem.App/ViewModels/Activity/ActivityEditViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using InformationSystem.App.Messages;
+using InformationSystem.App.Services.Interfaces;
+using InformationSystem.BL.Facades.Interfaces;
+using InformationSystem.BL.Models;
+using InformationSystem.Common.Enums;
+
+namespace InformationSystem.App.ViewModels.Activity;
+
+public partial class ActivityEditViewModel(
+    IActivityFacade activityFacade,
+    ICourseFacade courseFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService
+) : ViewModelBase(messengerService)
+{
+    [ObservableProperty]
+    private ActivityDetailModel activity = ActivityDetailModel.Empty;
+    
+    [ObservableProperty] 
+    private ObservableCollection<CourseListModel> courses = [];
+    
+    public CourseListModel? SelectedCourse { get; set; } = null;
+
+    public IEnumerable<ActivityType> ActivityTypes { get; set; } = new[]
+    {
+        ActivityType.Exam,
+        ActivityType.Exercise,
+        ActivityType.ComputerExercise,
+    };
+    
+    protected override async Task LoadDataAsync()
+    {
+        var allCourses = await courseFacade.GetAsync();
+        foreach (var courseListModel in allCourses)
+        {
+            Courses.Add(courseListModel);
+        }
+    }
+    
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        if (SelectedCourse != null)
+        {
+            Activity.CourseId = SelectedCourse.Id;
+            await activityFacade.SaveAsync(Activity);
+            
+            MessengerService.Send(new MessageAddActivity() {ActivityId = Activity.Id});
+            navigationService.SendBackButtonPressed();
+        }
+    }
+
+    public void Receive(MessageEditStudent message)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ InformationSystem/InformationSystem.App/Views/Activity/ActivityEditView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InformationSystem.App.ViewModels.Activity;
+
+namespace InformationSystem.App.Views.Activity;
+
+public partial class ActivityEditView
+{
+    public ActivityEditView(ActivityEditViewModel editViewModel) : base(editViewModel)
+    {
+        InitializeComponent();
+    }
+}
