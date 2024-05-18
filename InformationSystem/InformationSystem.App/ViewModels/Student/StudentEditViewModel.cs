d1383267abe66424518716a603ec4e74a91304c0
Maksim Dubrovin <maximus27rus@gmail.com>
2024-05-18 23:14:19 +0200

    Edit view now checks if Student could be added, or if we can add new course to him.

--- InformationSystem/InformationSystem.App/ViewModels/Student/StudentEditViewModel.cs
+++ InformationSystem/InformationSystem.App/ViewModels/Student/StudentEditViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Windows.Media.Devices.Core;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using InformationSystem.App.Messages;
 using InformationSystem.App.Services;
@@ -19,16 +20,53 @@ public partial class StudentEditViewModel(
     [ObservableProperty]
     private StudentDetailModel student = StudentDetailModel.Empty;
 
-    [RelayCommand]
+    [ObservableProperty] 
+    private string? firstName = null;
+    [ObservableProperty] 
+    private string? lastName = null;
+    [ObservableProperty] 
+    private string? group = null;
+    [ObservableProperty] 
+    private int currentYear;
+
+    partial void OnFirstNameChanged(string? value) => SaveCommand.NotifyCanExecuteChanged();
+    partial void OnLastNameChanged(string? value) => SaveCommand.NotifyCanExecuteChanged();
+    partial void OnGroupChanged(string? value) => SaveCommand.NotifyCanExecuteChanged();
+    partial void OnCurrentYearChanged(int value) => SaveCommand.NotifyCanExecuteChanged();
+
+    protected override Task LoadDataAsync()
+    {
+        FirstName = Student.FirstName;
+        LastName = Student.LastName;
+        Group = Student.Group;
+        CurrentYear = Student.CurrentYear;
+        
+        return base.LoadDataAsync();
+    }
+
+    [RelayCommand(CanExecute = nameof(CanSave))]
     private async Task SaveAsync()
     {
+        Student.FirstName = FirstName!;
+        Student.LastName = LastName!;
+        Student.Group = Group!;
+        Student.CurrentYear = CurrentYear;
+        
         await studentFacade.SaveAsync(Student);
         
         MessengerService.Send(new MessageEditStudent() {StudentId = Student.Id});
         navigationService.SendBackButtonPressed();
     }
 
-    [RelayCommand]
+    private bool CanSave()
+    {
+        return !string.IsNullOrEmpty(FirstName) 
+               && !string.IsNullOrEmpty(LastName)
+               && !string.IsNullOrEmpty(Group)
+               && CurrentYear > 0;
+    }
+
+    [RelayCommand(CanExecute = nameof(CanAddCourses))]
     private async Task GoToStudentCourseEditAsync()
     {
         await navigationService.GoToAsync("/courses", 
@@ -36,5 +74,10 @@ public partial class StudentEditViewModel(
             {
                 [nameof(StudentCoursesEditViewModel.Student)] = Student
             });
+    }
+
+    private bool CanAddCourses()
+    {
+        return Student.Id != Guid.Empty;
     }
 }
