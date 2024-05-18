5969ebe426b1fb094684d38981ad8934212a6ce8
Maksim Dubrovin <maximus27rus@gmail.com>
2024-05-18 23:11:42 +0200

    Made Activity detail view.

---
+++ InformationSystem/InformationSystem.App/ViewModels/Activity/ActivityDetailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using InformationSystem.App.Services.Interfaces;
+using InformationSystem.BL.Facades.Interfaces;
+using InformationSystem.BL.Models;
+
+namespace InformationSystem.App.ViewModels.Activity;
+
+[QueryProperty(nameof(Id), nameof(Id))]
+public partial class ActivityDetailViewModel(
+    IActivityFacade activityFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService
+) : ViewModelBase(messengerService)
+{
+    public Guid Id { get; set; }
+    
+    [ObservableProperty]
+    private ActivityDetailModel? activity;
+    
+    protected override async Task LoadDataAsync()
+    {
+        Activity = await activityFacade.GetAsync(Id);
+    }
+}
