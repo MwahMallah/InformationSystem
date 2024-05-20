a6586c1a12bf5491219c44e378c6ac37d3607a08
Maksim Dubrovin <maximus27rus@gmail.com>
2024-05-21 01:32:32 +0200

    added evaluation edit view.

---
+++ InformationSystem/InformationSystem.App/ViewModels/Evaluation/EvaluationEditViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using InformationSystem.App.Messages;
+using InformationSystem.App.Services.Interfaces;
+using InformationSystem.BL.Facades.Interfaces;
+using InformationSystem.BL.Models;
+
+namespace InformationSystem.App.ViewModels.Evaluation;
+
+[QueryProperty(nameof(Activity), nameof(Activity))]
+[QueryProperty(nameof(CourseFromQuery), nameof(CourseFromQuery))]
+public partial class EvaluationEditViewModel(
+    IActivityFacade activityFacade,
+    IStudentFacade studentFacade,
+    ICourseFacade courseFacade,
+    IEvaluationFacade evaluationFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService
+) : ViewModelBase(messengerService)
+{
+    [ObservableProperty]
+    private EvaluationDetailModel evaluation = EvaluationDetailModel.Empty;
+    
+    [ObservableProperty] 
+    private ObservableCollection<CourseListModel> courses = [];
+
+    [ObservableProperty] 
+    private ObservableCollection<ActivityListModel> activities = [];
+    
+    [ObservableProperty] 
+    private ObservableCollection<StudentListModel> students = [];
+    
+    [ObservableProperty]
+    private CourseListModel? selectedCourse = null;
+    
+    public CourseListModel? CourseFromQuery {get; set; } = null;
+
+    partial void OnSelectedCourseChanged(CourseListModel? value)
+    {
+        ChangeAvailableActivities();
+        ChangeAvailableStudents();
+        SaveCommand.NotifyCanExecuteChanged();
+    }
+    
+    
+    protected override async Task LoadDataAsync()
+    {
+        var allCourses = await courseFacade.GetAsync();
+        foreach (var courseListModel in allCourses)
+        {
+            Courses.Add(courseListModel);
+        }
+
+        if (CourseFromQuery != null)
+        {
+            SelectedCourse = CourseFromQuery;
+        }
+    }
+
+    private async void ChangeAvailableActivities()
+    {
+        Activities = new ObservableCollection<ActivityListModel>
+            (await activityFacade.GetFromCourseAsync(selectedCourse.Id));
+    }
+    
+    private async void ChangeAvailableStudents()
+    {
+        Students = new ObservableCollection<StudentListModel>
+            (await studentFacade.GetCourseStudentsAsync(selectedCourse.Id));
+    }
+    
+    [RelayCommand(CanExecute = nameof(CanSave))]
+    private async Task SaveAsync()
+    {
+        if (SelectedCourse != null)
+        {
+         
+            navigationService.SendBackButtonPressed();
+        }
+    }
+    
+    private bool CanSave()
+    {
+        return SelectedCourse != null;
+    }
+}
---
+++ InformationSystem/InformationSystem.App/Views/Evaluation/EvaluationEditView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InformationSystem.App.ViewModels.Evaluation;
+
+namespace InformationSystem.App.Views.Evaluation;
+
+public partial class EvaluationEditView 
+{
+    public EvaluationEditView(EvaluationEditViewModel viewModel): base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
