c43f651d25d0800918ee24694acfd2b071f28826
Maksim Dubrovin <maximus27rus@gmail.com>
2024-05-21 14:57:54 +0200

    Added filtering and sorting to Evaluation facade.

--- InformationSystem/InformationSystem.BL/Facades/EvaluationFacade.cs
+++ InformationSystem/InformationSystem.BL/Facades/EvaluationFacade.cs
@@ -14,6 +14,58 @@ public class EvaluationFacade(IUnitOfWorkFactory unitOfWorkFactory,
     : FacadeBase<EvaluationEntity, EvaluationDetailModel, 
             EvaluationListModel, EvaluationEntityMapper>(unitOfWorkFactory, modelMapper), IEvaluationFacade
 {
+    public override async Task<IEnumerable<EvaluationListModel>> GetAsync()
+    {
+        return await GetAsync(null, null);
+    }
+    
+    public async Task<IEnumerable<EvaluationListModel>> GetAsync(
+        string? searchQuery = null, string? orderQuery = null, bool isAscending = true)
+    {
+        await using var uow = unitOfWorkFactory.Create();
+        var repository = uow.GetRepository<EvaluationEntity, EvaluationEntityMapper>();
+        
+        IQueryable<EvaluationEntity> query = repository.Get()
+            .Include(e=>e.Student)
+            .Include(e => e.Activity)
+            .ThenInclude(a => a.Course);
+        
+        //TODO: sorting and filtering
+        if (!string.IsNullOrEmpty(searchQuery))
+        {
+            // Finds Evaluation that has matching student FullName or Course Abbreviation or number of points
+            query = query.Where(e => 
+                (e.Activity.Course.Abbreviation.StartsWith(searchQuery.ToLower())) ||
+                (e.Student.FirstName.ToLower().StartsWith(searchQuery)) ||
+                (e.Student.LastName.ToLower().StartsWith(searchQuery)));
+        }
+        
+        if (!string.IsNullOrEmpty(orderQuery))
+        {
+            query = orderQuery.ToLower() switch
+            {
+                "course_abbreviation" => isAscending 
+                    ? query.OrderBy(e => e.Activity!.Course!.Abbreviation) 
+                    : query.OrderByDescending(e => e.Activity!.Course!.Abbreviation),
+                "activity_type" => isAscending 
+                    ? query.OrderBy(e => e.Activity.ActivityType) 
+                    : query.OrderByDescending(e => e.Activity.ActivityType),
+                "full_name" => isAscending
+                    ? query.OrderBy(e => e.Student.LastName 
+                                         + " " + e.Student.FirstName)
+                    : query.OrderByDescending(e => e.Student.LastName 
+                                                   + " " + e.Student.FirstName),
+                "points" => isAscending 
+                    ? query.OrderBy(e => e.Points) 
+                    : query.OrderByDescending(e => e.Points),
+                _ => query
+            };
+        }
+        
+        var entities = await query.ToListAsync();
+        return modelMapper.MapToListModel(entities);
+    }
+    
     public override async Task<EvaluationDetailModel?> GetAsync(Guid id)
     {
         await using var uow = unitOfWorkFactory.Create();
--- InformationSystem/InformationSystem.BL/Facades/Interfaces/IEvaluationFacade.cs
+++ InformationSystem/InformationSystem.BL/Facades/Interfaces/IEvaluationFacade.cs
@@ -5,5 +5,6 @@ namespace InformationSystem.BL.Facades.Interfaces;
 
 public interface IEvaluationFacade : IFacade<EvaluationEntity, EvaluationDetailModel, EvaluationListModel>
 {
-    
+    public Task<IEnumerable<EvaluationListModel>> GetAsync(
+        string? searchQuery = null, string? orderQuery = null, bool isAscending = true);
 }
